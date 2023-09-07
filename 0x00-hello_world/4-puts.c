2b52e47e3e3e1f7b31fa5da3793bfabbe4b90cb5
j1Harrison <mfonobong625@gmail.com>
2023-09-07 04:12:52 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main -it prints out output using put 
+ * main - it prints out output using put
  *
  * Return: always (0)
  *
  */
 int main(void)
 {
-	puts("\"Programming is like buulding a multilingual puzzle\");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
