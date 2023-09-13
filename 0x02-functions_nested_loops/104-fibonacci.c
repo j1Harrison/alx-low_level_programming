b15f13d54b1058893206f65c0f7f409b94506cc4
j1Harrison <mfonobong625@gmail.com>
2023-09-13 04:54:40 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,38 +2,29 @@
 
 /**
  * main - prints the first 98 fibonaci sequences
- * Return: (0)
+ * Return: 0
  */
+
 int main(void)
 {
-	unsigned long int i, j, k, j1, j2, k1, k2;
+	int a = 1, b = 2;
 
-	j = 1;
-	k = 2;
+	printf("%d, %d, ", a, b);
 
-	printf("%lu", j);
+	int count = 2;
 
-	for (i = 1; i < 91; i++)
+	while (count < 98)
 	{
-		printf(",%lu", k);
-		k = k + j;
-		j = k - j;
-	}
-	j1 = j / 1000000000;
-	j2 = j % 1000000000;
-	k1 = k / 1000000000;
-	k2 = k % 1000000000;
 
-	for (i = 92; i < 99; i++)
-	{
-		printf(",%lu", k1 + (k2 / 1000000000));
-		printf("%lu", k2 % 1000000000);
-		k1 = k1 + j1;
-		j1 = k1 - j1;
-		k2 = k2 + j2;
-		j2 = k2 - j2;
-	}
+	int c = a + b;
 
+	printf("%d, ", c);
+
+	a = b;
+	b = c;
+
+	count++;
+	}
 	printf("\n");
 
 	return (0);
