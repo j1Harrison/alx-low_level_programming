bfc9f2c6926bae46d53e55dd5d2b94f1d05a44ec
j1Harrison <mfonobong625@gmail.com>
2023-09-14 06:28:40 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _upper - detect upper case alphabets
+ *
+ * @c: input character
+ *
+ * Return: (1) for true (0) false
+ *
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
