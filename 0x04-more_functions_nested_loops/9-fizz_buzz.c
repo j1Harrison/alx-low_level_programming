122e2d89025fa9c65d423d11a7dff92ff847533f
j1Harrison <mfonobong625@gmail.com>
2023-09-15 04:29:18 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 3 == 0 && (i % 5 == 0))
 		{
@@ -30,6 +30,7 @@ int main(void)
 		}
 
 	}
+	printf("Buzz");
 	printf("\n");
 
 	return (0);
