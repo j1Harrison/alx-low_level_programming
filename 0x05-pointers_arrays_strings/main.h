abf1e19274b1a12447d3f6972123dadf5fb43d43
j1Harrison <mfonobong625@gmail.com>
2023-09-18 06:24:04 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
-
+nt _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -14,5 +14,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _putchar(char c);
+
 #endif
