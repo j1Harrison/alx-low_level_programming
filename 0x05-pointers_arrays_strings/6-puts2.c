2622a1b44eb870f62215774f29169f1b9b00fb15
j1Harrison <mfonobong625@gmail.com>
2023-09-18 06:53:24 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -15,15 +15,15 @@ void puts2(char *str)
 	int o;
 
 	while (*y != '\0')
-        {
+	{
 		y++;
 		longi++;
 	}
 	t = longi - 1;
 
 	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
+	{	
+	if (o % 2 == 0)
 	{
 	_putchar(str[o]);
 	}
